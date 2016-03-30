b136fd00fbb9b1714ef736da7d02226e39a770f1
HeeHoon Kim <csehydrogen@gmail.com>
2016-03-30 19:11:44 +0900

    Ready for hw1

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function start() {
     return;
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.5, 0.5, 0.5, 1.0);
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -128,8 +128,27 @@ var modelWheel = null;
 var modelTrack = null;
 function initBuffers() {
   modelHull = getModel(modelData.hull, getConstantColor(modelData.hull.n, 0.5, 0.7, 0.7, 1));
-  modelTurret = getModel(modelData.turret, getConstantColor(modelData.turret.n, 0.7, 0.5, 0.7, 1));
-  modelGun = getModel(modelData.gun, getConstantColor(modelData.gun.n, 0.7, 0.7, 0.5, 1));
+  modelTurret = getModel(modelData.turret, getConstantColor(modelData.turret.n, 1, 0.4, 0.7, 1));
+  modelGun = getModel(modelData.gun, getConstantColor(modelData.gun.n, 1, 0, 0, 1));
+  const wheelColor = [];
+  for (var i = 0; i < modelData.wheel.n; ++i) {
+    for (var j = 0; j < 3; ++j) {
+      wheelColor.push(1);
+      wheelColor.push(i / 64);
+      wheelColor.push(i / 64);
+      wheelColor.push(1);
+      wheelColor.push(i / 64);
+      wheelColor.push(1);
+      wheelColor.push(i / 64);
+      wheelColor.push(1);
+      wheelColor.push(i / 64);
+      wheelColor.push(i / 64);
+      wheelColor.push(1);
+      wheelColor.push(1);
+    }
+  }
+  modelWheel = getModel(modelData.wheel, wheelColor);
+  modelTrack = getModel(modelData.track, getConstantColor(modelData.track.n, 0.25, 0.25, 0.25, 1));
 }
 
 function resize() {
@@ -141,20 +160,6 @@ function resize() {
     gl.canvas.height = height;
     gl.viewport(0, 0, width, height);
   }
-}
-
-var lastTime = null;
-var angle = 0.0;
-function animate() {
-  const currentTime = Date.now();
-  if (lastTime) {
-    const delta = currentTime - lastTime;
-    angle += Math.PI * delta / 1000;
-    if (angle > Math.PI * 2) {
-      angle -= Math.PI * 2;
-    }
-  }
-  lastTime = currentTime;
 }
 
 function setUniforms() {
@@ -190,49 +195,131 @@ function keyupListener(e) {
   console.log(e.keyCode);
 }
 
-var pXangle = 0, pYangle = 0, pXpos = 0, pYpos = 0;
-function handleInput() {
-  if (keystate[68]) pXpos -= 1 / 60;
-  if (keystate[65]) pXpos += 1 / 60;
-  if (keystate[87]) pYpos -= 1 / 60;
-  if (keystate[83]) pYpos += 1 / 60;
-  if (keystate[84]) pXangle -= Math.PI / 2 / 60;
-  if (keystate[71]) pXangle += Math.PI / 2 / 60;
-  if (keystate[70]) pYangle -= Math.PI / 2 / 60;
-  if (keystate[72]) pYangle += Math.PI / 2 / 60;
+var lastTime = null;
+var pXangle = Math.PI / 8, pYangle = -Math.PI / 4, pPos = 10, wOfs = 0;
+var tAngle = 0, gAngle = 0, auto = false;
+const wRadius = 0.2;
+function animate() {
+  const currentTime = Date.now();
+  if (lastTime) {
+    const delta = (currentTime - lastTime) / 1000;
+    if (keystate[84]/*t*/) pXangle += Math.PI / 2 * delta;
+    if (keystate[71]/*g*/) pXangle -= Math.PI / 2 * delta;
+    if (keystate[70]/*f*/) pYangle += Math.PI / 2 * delta;
+    if (keystate[72]/*h*/) pYangle -= Math.PI / 2 * delta;
+    if (keystate[82]/*r*/) pPos -= 5 * delta;
+    if (keystate[89]/*y*/) pPos += 5 * delta;
+    if (keystate[65]/*a*/) tAngle += Math.PI / 2 * delta;
+    if (keystate[68]/*d*/) tAngle -= Math.PI / 2 * delta;
+    if (keystate[83]/*s*/) gAngle += Math.PI / 9 * delta;
+    if (keystate[87]/*w*/) gAngle -= Math.PI / 9 * delta;
+    if (keystate[90]/*z*/) auto = true;
+    if (keystate[88]/*x*/) auto = false;
+    if (keystate[67]/*c*/) wOfs += 0.5 * delta;
+    if (auto) {
+      const amp = Math.PI / 18, w = Math.PI * 2;
+      tAngle += amp * w * Math.cos(w * currentTime / 1000) * delta;
+      gAngle += amp * w * Math.sin(w * currentTime / 1000) * delta;
+      wOfs += 0.5 * delta;
+    }
+    if (gAngle > Math.PI / 18) gAngle = Math.PI / 18;
+    if (gAngle < -Math.PI / 9) gAngle = -Math.PI / 9;
+  }
+  lastTime = currentTime;
 }
 
 function drawScene() {
   resize();
-  handleInput();
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   const pMat = mat4.create();
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   mat4.perspective(pMat, 45, aspect, 0.1, 100.0);
-  mat4.translate(pMat, pMat, [pXpos, pYpos, 0]);
+  mat4.translate(pMat, pMat, [0, 0, -pPos]);
   mat4.rotate(pMat, pMat, pXangle, [1, 0, 0]);
   mat4.rotate(pMat, pMat, pYangle, [0, 1, 0]);
   gl.uniformMatrix4fv(uPMatrix, false, pMat);
 
-  pushMatrix();
-  translate([0.0, 0.0, -5.0]);
-    pushMatrix();
-    setUniforms();
+  function drawTank() {
     setModel(modelHull);
+    setUniforms();
     drawModel(modelHull);
+
+    pushMatrix();
+    translate([0, 0.2, 0.3]);
+    rotate(tAngle, [0, 1, 0]);
+    setModel(modelTurret);
+    setUniforms();
+    drawModel(modelTurret);
+    translate([0, 0.15, 0.5]);
+    rotate(gAngle, [1, 0, 0]);
+    setModel(modelGun);
+    setUniforms();
+    drawModel(modelGun);
     popMatrix();
 
-    translate([0, 0.2, 0.2]);
-    setUniforms();
-    setModel(modelTurret);
-    drawModel(modelTurret);
+    pushMatrix();
+    translate([-0.6, -0.25, -0.9]);
+    setModel(modelWheel);
+    for (var i = 0; i < 2; ++i) {
+      for (var j = 0; j < 6; ++j) {
+        pushMatrix();
+        rotate(wOfs / 0.4 * 2.0 / wRadius, [1, 0, 0]);
+        setUniforms();
+        drawModel(modelWheel);
+        popMatrix();
+        translate([0, 0, 0.4]);
+      }
+      translate([1.2, 0, -2.4]);
+    }
+    popMatrix();
 
-    translate([0, 0.15, 0.6]);
-    setUniforms();
-    setModel(modelGun);
-    drawModel(modelGun);
+    pushMatrix();
+    translate([-0.6, 0, 0]);
+    setModel(modelTrack);
+    for (var i = 0; i < 2; ++i) {
+      for (var j = 0; j < 34; ++j) {
+        var ofs = (wOfs + j / 34) % 1;
+        pushMatrix();
+        if (ofs <= 0.4) {
+          translate([0, -0.25 + wRadius, -0.9 + ofs / 0.4 * 2.0]);
+        } else if (ofs <= 0.5) {
+          const rad = Math.PI * (ofs - 0.4) / 0.1;
+          translate([
+            0,
+            -0.25 + wRadius * Math.cos(rad),
+            1.1 + wRadius * Math.sin(rad)
+          ]);
+          rotate(rad, [1, 0, 0]);
+        } else if (ofs <= 0.9) {
+          translate([0, -0.25 - wRadius, 1.1 - (ofs - 0.5) / 0.4 * 2.0]);
+        } else {
+          const rad = Math.PI * (ofs - 0.9) / 0.1;
+          translate([
+            0,
+            -0.25 - wRadius * Math.cos(rad),
+            -0.9 - wRadius * Math.sin(rad)
+          ]);
+          rotate(rad, [1, 0, 0]);
+        }
+        setUniforms();
+        drawModel(modelTrack);
+        popMatrix();
+      }
+      translate([1.2, 0, 0]);
+    }
+    popMatrix();
+  }
+
+  pushMatrix();
+  for (var i = 0; i < 4; ++i) {
+    rotate(Math.PI / 2, [0, 1, 0]);
+    pushMatrix();
+    translate([3, 0, 0]);
+    drawTank();
+    popMatrix();
+  }
   popMatrix();
 
   animate();
